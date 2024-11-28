0484cdbb6a3201e29d424262b703ed12c5c37fed
kumudun <kumudun@metropolia.fi>
2024-11-29 00:56:59 +0200

    commit

--- Module1/task5/task5.js
+++ Module1/task5/task5.js
@@ -11,6 +11,6 @@ if (year % 4 === 0 && year % 100 !== 0 || year % 400 === 0) {
   leap = 'is';
 }
 
-const result = `Year ${year} ${year} a leap year`;
+const result = `Year ${year}   a leap year`;
 
 target.innerHTML = result;
